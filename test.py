18e5b49f72c1299c347bd4e05b7901a48bb1a1a7
Manan Sharma <msharma3732@gmail.com>
2025-06-03 16:36:27 +0530

    Update test.py

--- test.py
+++ test.py
@@ -106,4 +106,4 @@ if uploaded_file is not None:
 
     predict1 = st.button("Predict Species")
     if predict1:
-      st.success(model.predict(input_data))
+      st.write(model.predict(input_data))
